0a8d60688c061abaea06ce750283081ffcb394e5
ssony <ssony@cinamon.io>
2024-06-12 13:09:51 +0900

    Chage_Folder Name

---
+++ UPython/Control_Material/CheckToMaterialValue.py
@@ -0,0 +1,31 @@
+
+import unreal
+
+selected_assets = unreal.EditorUtilityLibrary.get_selected_assets()
+
+# 확인하고자 하는 파라미터 이름
+parameter_name = "UseWorldMapping"
+
+# get boolean parameter
+for asset in selected_assets:
+    if isinstance(asset, unreal.MaterialInstance):
+        # 파라미터 이름과 값을 가져오기
+        found = unreal.MaterialEditingLibrary.get_material_instance_static_switch_parameter_value(asset, parameter_name)
+        if found:
+            print(f"Asset: {asset.get_name()}", parameter_name, ">>>", found )
+        else: 
+            pass
+    else:
+        pass
+
+
+# # get scalar parameter
+# for asset in selected_assets:
+#     if isinstance(asset, unreal.MaterialInstance):
+#         found = unreal.MaterialEditingLibrary.get_material_instance_scalar_parameter_value(asset,parameter_name)
+#         if found:
+#             print(f"Asset: {asset.get_name()}", parameter_name, ">>>", found )
+#         else: 
+#             pass
+#     else:
+#         pass
